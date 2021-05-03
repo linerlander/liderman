85a6e089719ded44876003b310c7e4739fb5203f
linerlander <51091612+linerlander@users.noreply.github.com>
2021-05-03 01:33:15 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Liderman</title>
+    <link rel="stylesheet" href="css/estilosss.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800&display=swap" rel="stylesheet"> 
+</head>
+
+<body>
+    <header>
+        <nav id="menu">
+            <ul>
+                <li><a href="paginas/tramitesenlinea.html">Trámites en Línea</a></li>
+                <li class="barra">|</li>
+                <li><a href="paginas/ofertaslaborales.html">Ofertas Laborales</a></li>
+             
+
+               
+            </ul>
+        
+        </nav>
+        <div class="container">
+            <nav>
+                <div class="enlaces uno" id="enlaces">
+                    <a href="">Inicio</a>
+                    <a href="">Acerca de</a>
+                    <a href="">Contacto</a>
+                </div>
+            </nav>
+            <div class="textos">
+                <a href="#">LIDERMAN</a>
+                <br>
+                <h1>SÉ PARTE DEL</h1>
+                <h2>EQUIPO LIDERMAN</h2>
+                
+            </div>
+            <img src="imagenes/equipo.png" alt="100">
+        </div>
+    </header>
+    <div class="wave">
+        <div style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,49.98 C150.00,150.00 349.20,-50.00 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #fff;"></path></svg></div>
+    </div>
+    <br>
+    <br>
+    <section class="main">
+         <div class="contenedora">
+                <h1>APRENDE DE LOS EXPERTOS EN SEGURIDAD<span>&#160;</span></h1>
+         </div>            
+    </section>
+    
+    <aside>
+        <div class="contenedor">
+            <div class="circulo">
+                <a>¿Cómo es el proceso de selección?</a>
+                <video src="video/video.mp4" autoplay loop></video>
+            </div>
+
+
+            <div class="cuadrado">
+                <a href="paginas/ofertaslaborales.html">Ofertas Laborales</a>
+                <img src="imagenes/pointer.jpg">
+            </div>
+
+        </div>
+        <br>
+        <div class="contenedor">
+            <div class="caja">
+                <h1>Soy Postulante</h1>
+                <img class="imagen1" src="imagenes/escuelalider.png">
+                <form action="validar.php" method="post">
+
+                    <input type="text" name="usuario" placeholder="Enter Username" required=""> 
+                    <input type="password" placeholder="Enter Password" required="" name="contraseña">
+
+                    <input type="submit" value="INGRESAR">
+
+                    
+                </form>
+            </div>
+            <div class="triangulo">
+                <a href="paginas/tramitesenlinea.html">Trámites en Línea</a>
+                <img src="imagenes/pointer.jpg">
+            </div>
+        </div>
+        <br>
+        
+        <div class="contenedor">
+            <div class="rombo">
+                <a href="https://us02web.zoom.us/meeting/register/tZMsceygrDsqGdEf6FO_yPzWuv8671mt5v-u" target="_blank">Charla Virtual Zoom</a>
+                <img src="imagenes/zoom.png">
+            </div>
+            <div class="rectangulo">
+                <a href="paginas/whatsapp.html">Contacta al responsable de selección</a>
+                <img src="imagenes/pointer.jpg">
+            </div>
+        </div>
+        <br>
+        <br>
+          
+    </aside>
+    <footer class="footer">
+     <div class="container">
+        <div class="row">
+            <div class="footer-col">
+                <h4>Liderman</h4>
+                <ul>
+                    <li><a href="http://www.liderman.com.pe/wp-content/uploads/2019/08/POLITICA-DE-PRIVACIDAD-LIDERMAN.pdf" target="_blank">✔️Política y Privacidad</a></li>
+                    <li><a href="#">✔️Teléfono </a></li>
+                    <li><a href="https://www.google.com/maps/place/Av.+Defensores+del+Morro+1620,+Chorrillos+15066/@-12.1779543,-77.0168484,19z/data=!4m13!1m7!3m6!1s0x9105b7792d3c0917:0xa353e06ca4591502!2sAv.+Defensores+del+Morro+1620,+Chorrillos+15066!3b1!8m2!3d-12.1777835!4d-77.0162111!3m4!1s0x9105b7792d3c0917:0xa353e06ca4591502!8m2!3d-12.1777835!4d-77.0162111" target="_blank">✔️Ubicación</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>Redes Sociales</h4>
+                <ul>
+                    <li><a href="https://www.facebook.com/LidermanOficial/?brand_redir=1355012104618772" target="_blank">✔️Facebook</a></li>
+                    <li><a href="https://pe.linkedin.com/company/liderman" target="_blank">✔️Linkedin</a></li>
+                    <li><a href="https://www.computrabajo.com.pe/lidermanperu/empleos" target="_blank">✔️Computrabajo</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>tramites online </h4>
+                <ul>
+                    <li><a href="https://www.policia.gob.pe/home/CerapOnLine" target="_blank">✔️Antecedentes Policiales</a></li>
+                    <li><a href="https://cape.pj.gob.pe/cape/" target="_blank">✔️Antecedentes Penales</a></li>
+                    <li><a href="https://apps.inpe.gob.pe/certificado_aj_electronico/registroCE.html" target="_blank">✔️Antecedentes Judiciales</a></li>
+                </ul>
+            </div>
+             <div class="footer-col">
+                <img src="imagenes/lidder.png">
+            </div>
+        </div>
+      </div>
+    </footer>
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
